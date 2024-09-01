6d6c7640acd5591913bdccd720f474d2a6700dbc
Raphael Maswi <raphael.maswi@crdbbank.co.tz>
2024-09-01 10:04:33 +0300

    added post date display

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -1,8 +1,18 @@
 import Link from "next/link";
 import { PostMetadata } from "./PostMetadata";
 import Image from "next/image";
+import moment from "moment";
 
 const PostPreview = (props: PostMetadata) => {
+  function getMomentString(postTime: string): string {
+    let postMoment = moment(postTime);
+    if (postMoment.isAfter(moment().subtract(1, "day"))) {
+      return postMoment.fromNow();
+    } else {
+      return postMoment.format("ll");
+    }
+  }
+
   return (
     <div className="p-4  transition duration-200 transform hover:translate-y-2 ">
       <div className="hover:opacity-80">
@@ -20,6 +30,10 @@ const PostPreview = (props: PostMetadata) => {
         <p className="text-sm pt-3 tracking-wider font-medium pr-1">By</p>
         <p className="text-sm pt-3 text-[#e53170] tracking-wider font-semibold">
           {props?.source_name}
+        </p>
+        <span className="flex-auto"></span>
+        <p className="text-sm pt-3 tracking-wider font-medium pr-1">
+          {getMomentString(props?.pubDate)}
         </p>
       </div>
       <p className="text-sm pt-3 text-gray-600 tracking-wider">
